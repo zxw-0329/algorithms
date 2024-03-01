debd33755cdb045a475954fc1ff338232a5ddd87
zxw-0329 <157824991+zxw-0329@users.noreply.github.com>
2024-03-01 17:19:56 +0800

    75311D

---
+++ math/75311D.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+void solve() {
+    int n;
+    cin >> n;
+    ll cnt = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x == 0)cnt++;
+    }
+    cout << cnt * cnt << "\n";
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    //  cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
