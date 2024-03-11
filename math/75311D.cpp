e1b1cd1712d02e4667070da2a78b8f3319b25485
zxw-0329 <157824991+zxw-0329@users.noreply.github.com>
2024-03-11 19:50:35 +0800

    75311D

--- math/75311D.cpp
+++ math/75311D.cpp
@@ -1,6 +1,13 @@
 /*
-
- 
+   lcm(x,y) = k * max(x,y) , k为整数
+   又 x ^ y < 2 * max(x,y) ;
+   若两者相等 只有可能 k = 1 ;
+   所以 lcm(x,y) = max(x,y) = x^ y;
+   即 x, y 中较大者是较小者的倍数;
+   令 x > y
+   所以 x 是 y 的倍数 且 x= x ^ y  ->  y=0;
+    x是y的倍数 -> x=0;
+    答案为 count(0)*count(0);
     https://ac.nowcoder.com/acm/contest/75311/D
 */
 #include<bits/stdc++.h>
