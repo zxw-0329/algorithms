df64be6a3aa85c45ab20864f3958d542ff602dd3
zxw-0329 <157824991+zxw-0329@users.noreply.github.com>
2024-04-10 23:02:15 +0800

    前缀和

---
+++ math/Special Elements.cpp
@@ -0,0 +1,48 @@
+/*
+  前缀和 + 标记 +观察数据范围
+  对于每个数ai都是<=n
+  对于前缀和只需要标记 <=n 的数即可
+  之后在统计
+*/
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+void solve() {
+   int n;
+   cin >> n;
+   vector<int>a(n);
+   map<int, int>m;
+   for (int i = 0; i < n; i++) {
+      cin >> a[i];
+      m[a[i]]++;
+   }
+   vector<int>s(n + 1);
+   for (int i = 0; i < n; i++) {
+      s[i + 1] = s[i] + a[i];
+   }
+   int cnt = 0;
+   vector<bool>f(n + 1);
+   for (int i = 1; i <= n; i++) {
+      for (int j = i + 1; j <= n; j++) {
+         if (s[j] - s[i - 1] <= n) {
+            f[s[j] - s[i - 1]] = true;
+         }
+         
+      }
+   }
+   for (auto i : a) {
+      cnt += f[i];
+   }
+   cout << cnt << "\n";
+}
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   cout.tie(nullptr);
+   int t;
+   cin >> t;
+   while (t--) {
+      solve();
+   }
+   return 0;
+}
