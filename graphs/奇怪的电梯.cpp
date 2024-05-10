d3da8775e642f60b86303445f77e6f1a23ba9bc8
zxw-0329 <157824991+zxw-0329@users.noreply.github.com>
2024-05-10 17:15:26 +0800

    bfs

---
+++ graphs/奇怪的电梯.cpp
@@ -0,0 +1,48 @@
+/*
+  用题目所给定的信息建图
+  因为路径长度都为1
+  直接用bfs寻找到终点最小次数
+*/
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n, a, b;
+    while (cin >> n >> a >> b && n != 0) {
+        vector<int>e[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int x;
+            cin >> x;
+            if (i + x <= n) {
+                e[i].push_back(i + x);
+            }
+            if (i - x >= 1) {
+                e[i].push_back(i - x);
+            }
+        }
+        queue<pair<int, int>>q;
+        vector<int>vis(n + 1);
+        q.emplace(a, 0);
+        vis[a] = 1;
+        while (q.size()) {
+            auto [x, y] = q.front();
+            q.pop();
+            if (x == b) {
+                cout << y << "\n";
+                return 0;
+            }
+            for (auto v : e[x]) {
+                if (vis[v])continue;
+                q.emplace(v, y + 1);
+                vis[v] = 1;
+            }
+        }
+        cout << -1 << "\n";
+    }
+ 
+    return 0;
+}
