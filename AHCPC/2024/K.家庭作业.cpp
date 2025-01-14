4d0343048c3212e22e1478e17f7088a42f978958
zxw-0329 <157824991+zxw-0329@users.noreply.github.com>
2025-01-14 15:38:19 +0800

    数学，逆元

--- AHCPC/2024/K.家庭作业.cpp
+++ AHCPC/2024/K.家庭作业.cpp
@@ -1,107 +1,94 @@
-//正确性有待验证 缺少数据
 #include<bits/stdc++.h>
+
 using namespace std;
-using ll = long long;
-constexpr double eps = 1e-6;
-constexpr int mod = 998244353;
-constexpr int N = 100000 + 5;
-vector<int> minp, primes;
-unordered_map<int, int>d;
-int u = 0;
-void sieve(int n) {
-    minp.assign(n + 1, 0);
-    primes.clear();
 
-    for (int i = 2; i <= n; i++) {
-        if (minp[i] == 0) {
-            minp[i] = i;
-            primes.push_back(i);
-            d[i] = u++;
-        }
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
 
-        for (auto p : primes) {
-            if (i * p > n) {
-                break;
-            }
-            minp[i * p] = p;
-            if (p == minp[i]) {
-                break;
-            }
-        }
+int power(int a, i64 b, int m) {
+    if (b < 0) {
+        return 0;
     }
-}
-vector<pair<int, int>> get(int n) {
-    vector<pair<int, int>>ans;
-    for (int i = 2; i <= n; i++) {
-        int t = i;
-        for (auto p : primes) {
-            if (p * p > i)break;
-            if (t % p == 0) {
-                int cnt = 0;
-                while (t % p == 0) {
-                    cnt++;
-                    t /= p;
-                }
-                if (d[i] < ans.size()) {
-                    ans[d[i]].second += cnt * i;
-                }
-                else {
-                    ans.push_back({ p,cnt * i });
-                }
-            }
-        }
-        if (t > 1) {
-            if (d[t] < ans.size()) {
-                ans[d[i]].second += i;
-            }
-            else {
-                ans.push_back({ t,i });
-            }
-        }
-    }
-    return ans;
-}
-int qpow(int a, int b, int m) {
     int ans = 1;
     while (b) {
         if (b & 1) {
-            ans = ans * 1ll * a % m;
+            ans = ans * 1LL * a % m;
         }
         b >>= 1;
-        a = a * 1ll * a % m;
+        a = a * 1LL * a % m;
     }
-    return ans % m;
+    return ans;
+}
+int exgcd(int a, int b, int& x, int& y) {//a* x + b * y = 1; 求x
+    if (b == 0) {
+        x = 1, y = 0;
+        return a;
+    }
+    int d = exgcd(b, a % b, y, x);
+    y -= a / b * x;
+    return d;
+}
+int inv(int a,int m) {
+
+    int x, y;
+    int g = exgcd(a, m, x, y);
+
+    return (x % m + m) % m;
 }
 void solve() {
-    int n, m, q;
+
+    int n, q, r;
+    int m;
     cin >> n >> m >> q;
-    auto a = get(n);
-    while (q--) {
-        int r;
-        cin >> r;
-        auto b = get(r);
-        auto c = get(n - r);
-        ll ans = 1;
-        for (int i = 0; i < a.size(); i++) {
-            int t = a[i].second;
-            if (i < b.size()) {
-                t -= b[i].second;
+
+    vector<int>p;
+    int y = m;
+    for (int i = 2; i * 1LL * i <= y; i++) {
+        if (y % i == 0) {
+            p.push_back(i);
+            while (y % i == 0) {
+                y /= i;
             }
-            if (i < c.size()) {
-                t -= c[i].second;
-            }
-            if (t == 0)continue;
-            ans = ans * qpow(a[i].first, t, m) % m;
         }
-        cout << ans << "\n";
+    }
+    if (y > 1) {
+        p.push_back(y);
+    }
+
+    vector<int>g(n + 1);
+    vector<vector<i64>>cnt(n + 1, vector<i64>(p.size()));
+
+    vector<int>g_inv(n + 1);
+    g[1] = 1; g_inv[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        int x = i;
+        for (int j = 0; j < p.size(); j++) {
+            int c = 0;
+            while (x % p[j] == 0) {
+                x /= p[j];
+                c++;
+            }
+            cnt[i][j] = cnt[i - 1][j] + c * 1LL * i;
+        }
+        assert(gcd(x, m) == 1);
+        g[i] = g[i - 1] * 1LL * power(x, i, m) % m;
+        g_inv[i] = inv(g[i], m);
+    }
+
+    while (q--) {
+        cin >> r;
+        int s = 1;
+        for (int j = 0; j < p.size(); j++) {
+            s = (s * 1LL * power(p[j], cnt[n][j] - cnt[r][j] - cnt[n - r][j], m)) % m;
+        }
+        cout << (g[n] * 1LL * g_inv[r] % m * g_inv[n - r] % m * s) % m << "\n";
     }
 }
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    sieve(1E5);
-    //cout << primes.size() << "\n";
+
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
     int t;
     cin >> t;
     while (t--) {
